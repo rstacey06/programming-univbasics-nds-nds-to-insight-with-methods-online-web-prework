51f54af8fe83e98fb540801604c748e67261a273
burbage stacey <rstacey06@gmail.com>
2019-10-24 21:32:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,13 +1,83 @@
 require 'directors_database'
 
+def flatten_a_o_a(aoa)
+  result = []
+  i = 0
 
+  while i < aoa.length do
+    k = 0
+    while k < aoa[i].length do
+      result << aoa[i][k]
+      k += 1
+    end
+    i += 1
+  end
 
-def directors_totals(nds)
-  result = {}
-  nil
+ return result
 end
 
-# Find a way to accumulate the :worldwide_grosses and return that Integer
-# using director_data as input
-def gross_for_director(director_data)
+def movie_with_director_name(director_name, movie_data)
+  { 
+    :title => movie_data[:title],
+    :worldwide_gross => movie_data[:worldwide_gross],
+    :release_year => movie_data[:release_year],
+    :studio => movie_data[:studio],
+    :director_name => director_name
+  }
+end
+
+def movies_with_director_key(name, movies_collection)
+  
+ result = []
+  i = 0
+
+  while i < movies_collection.length do
+    movie_data = movies_collection[i]
+    result << movie_with_director_name(name, movie_data)
+    i += 1
+  end
+
+ return result
+end
+
+
+def gross_per_studio(collection)
+ 
+  result = {}
+  i = 0
+
+  while i < collection.length do
+    movie = collection[i]
+
+    if !result[movie[:studio]]
+      result[movie[:studio]] = movie[:worldwide_gross]
+    else
+      result[movie[:studio]] += movie[:worldwide_gross]
+    end
+    i += 1
+  end
+
+ return result
+end
+
+def movies_with_directors_set(source)
+ 
+  i = 0
+  a_o_a_movies_by_dir = []
+
+  while i < source.length do
+    dir_info_hash = source[i]
+    director_name = dir_info_hash[:name]
+    directors_movies = dir_info_hash[:movies]
+    a_o_a_movies_by_dir << movies_with_director_key(director_name, directors_movies)
+    i += 1
+  end
+
+ return a_o_a_movies_by_dir
+end
+
+def studios_totals(nds)
+  a_o_a_movies_with_director_names = movies_with_directors_set(nds)
+  movies_with_director_names = flatten_a_o_a(a_o_a_movies_with_director_names)
+  return gross_per_studio(movies_with_director_names)
 end
