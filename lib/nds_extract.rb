1f3a00aa8bc3cda72d7a64f78b728737d67d8162
burbage stacey <rstacey06@gmail.com>
2019-10-24 21:27:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,9 +1,6 @@
 require 'directors_database'
 
-# Write a method that, given an NDS creates a new Hash
-# The return value should be like:
-#
-# { directorOne => allTheMoneyTheyMade, ... }
+
 
 def directors_totals(nds)
   result = {}
